e8c668a3b61fe2ad0425193ec8de56140d91d65f
mcannood <maxime.cannoodt@ugent.be>
2022-04-28 15:58:16 +0200

    WIP

--- src/post_processing/hydroshoot/process_experiment.py
+++ src/post_processing/hydroshoot/process_experiment.py
@@ -10,27 +10,30 @@ from typing import Dict
 from pathlib import Path
 from shutil import copyfile
 
-from src.post_processing.extract_vertex_properties import get_time_series_for_class
+from src.post_processing.hydroshoot.extract_vertex_properties import (
+    get_time_series_for_class,
+)
+
 
 def get_file_paths(path: str) -> Dict[str, str]:
-  """Checks if the passed path contains all the required experiment inputs and outputs.
+    """Checks if the passed path contains all the required experiment inputs and outputs.
   Throws an exception if an input or output file is missing."""
-  paths = {
-    "input": os.path.join(path, 'meteo.input'),
-    "params": os.path.join(path, "params.json"),
-    "output_dir": os.path.join(path, "output/"),
-    "output_series": os.path.join(path, "output/time_series.output")
-  }
+    paths = {
+        "input": os.path.join(path, "meteo.input"),
+        "params": os.path.join(path, "params.json"),
+        "output_dir": os.path.join(path, "output/"),
+        "output_series": os.path.join(path, "output/time_series.output"),
+    }
 
-  for p in paths.values():
-    if not os.path.exists(p):
-      raise FileNotFoundError(f"No such file/directory: '{p}'")
+    for p in paths.values():
+        if not os.path.exists(p):
+            raise FileNotFoundError(f"No such file/directory: '{p}'")
 
-  return paths;
+    return paths
 
 
 def process_experiment(path: str, name: str, logger=None) -> str:
-  """Takes as input the root folder of a HydroShoot experiment and 
+    """Takes as input the root folder of a HydroShoot experiment and 
   outputs a directory 'results/' containing the following serialized objects: 
 
   "results/{experiment_name}/env_input.csv (Pandas dataframe containing the environmental inputs of the experiment)
@@ -40,49 +43,55 @@ def process_experiment(path: str, name: str, logger=None) -> str:
 
   Returns the output path as string.
   """
-  paths = get_file_paths(path)
+    paths = get_file_paths(path)
 
-  # Read environmental inputs
-  inputs = pd.read_csv(paths['input'], sep=';')
-  inputs = inputs[['time', 'Tac', 'u', 'hs', 'Rg']]
+    # Read environmental inputs
+    inputs = pd.read_csv(paths["input"], sep=";")
+    inputs = inputs[["time", "Tac", "u", "hs", "Rg"]]
 
-  # Read plant performance outputs
-  output_series = pd.read_csv(paths['output_series'], sep=';')
+    # Read plant performance outputs
+    output_series = pd.read_csv(paths["output_series"], sep=";")
 
-  # Extract all leaf-level series
-  timestamps, store = get_time_series_for_class(paths['output_dir'], 'L')
+    # Extract all leaf-level series
+    timestamps, store = get_time_series_for_class(paths["output_dir"], "L")
 
-  if logger:
-    logger.info(f'[{name}] Extracted {len(timestamps)} steps, {len(store)} properties, {len(list(store.values())[0])} state size')
-  else:
-    print(f'\nExtracted time series: '
-          f'\n\tfrom {timestamps[0].format("YYYY-MM-DD HH:mm")} to {timestamps[-1].format("YYYY-MM-DD HH:mm")}'
-          f'\n\t{len(timestamps):<4} steps'
-          f'\n\t{len(store):<4} properties'
-          f'\n\t{len(list(store.values())[0]):<4} state variables')
+    if logger:
+        logger.info(
+            f"[{name}] Extracted {len(timestamps)} steps, {len(store)} properties, {len(list(store.values())[0])} state size"
+        )
+    else:
+        print(
+            f"\nExtracted time series: "
+            f'\n\tfrom {timestamps[0].format("YYYY-MM-DD HH:mm")} to {timestamps[-1].format("YYYY-MM-DD HH:mm")}'
+            f"\n\t{len(timestamps):<4} steps"
+            f"\n\t{len(store):<4} properties"
+            f"\n\t{len(list(store.values())[0]):<4} state variables"
+        )
 
-  # Create destination folder if necessary
-  destination = os.path.join(os.getcwd(), f'results/{name}')
-  Path(destination).mkdir(parents=True, exist_ok=True)
+    # Create destination folder if necessary
+    destination = os.path.join(os.getcwd(), f"results/{name}")
+    Path(destination).mkdir(parents=True, exist_ok=True)
 
-  # Write all results to disk
-  inputs.to_csv(os.path.join(destination, 'env_input.csv'), index=False)
-  output_series.to_csv(os.path.join(destination, 'plant_outputs.csv'), index=False)
-  copyfile(paths['params'], os.path.join(destination, 'params.json'))
-  with open(os.path.join(destination, 'leaf_data.pickle'), 'wb') as f:
-    dump(store, f)
+    # Write all results to disk
+    inputs.to_csv(os.path.join(destination, "env_input.csv"), index=False)
+    output_series.to_csv(os.path.join(destination, "plant_outputs.csv"), index=False)
+    copyfile(paths["params"], os.path.join(destination, "params.json"))
+    with open(os.path.join(destination, "leaf_data.pickle"), "wb") as f:
+        dump(store, f)
 
-  return destination
+    return destination
+
 
 if __name__ == "__main__":
-  if len(sys.argv) < 3:
-    print("1st arg: path/to/experiment/,   2nd arg: experiment name")
-    exit()
-  path = sys.argv[1]
-  name = sys.argv[2]
-  if not os.path.isdir(path):
-    print(f"'{path}' is not a valid path.")
-    exit()
+    if len(sys.argv) < 3:
+        print("1st arg: path/to/experiment/,   2nd arg: experiment name")
+        exit()
+    path = sys.argv[1]
+    name = sys.argv[2]
+    if not os.path.isdir(path):
+        print(f"'{path}' is not a valid path.")
+        exit()
 
-  output_dir = process_experiment(path, name)
-  print(f'Results written to {output_dir}\n')
+    output_dir = process_experiment(path, name)
+    print(f"Results written to {output_dir}\n")
+
--- src/run/from_json.py
+++ src/run/from_json.py
@@ -23,72 +23,85 @@ from src.run.sim_process import run_experiment, check_path, configure_logging
 
 
 def get_args(fpath: str):
-	"""Loads the JSON object at fpath and generates an iterable
+    """Loads the JSON object at fpath and generates an iterable
 	of args for src.run.sim_process"""
-	args = []
-	with open(fpath) as f:
-		experiments = json.load(f)
-		if not isinstance(experiments, list):
-			raise TypeError("Experiment json file should contain a list of experiment objects.")
-		
-		for exp in experiments:
-			start, end, length = exp['sdate'], exp['edate'], exp['length']
-			start = arrow.get(start, 'YYYY-MM-DD')
-			end = arrow.get(end, 'YYYY-MM-DD')
-			length = int(length)
+    args = []
+    with open(fpath) as f:
+        experiments = json.load(f)
+        if not isinstance(experiments, list):
+            raise TypeError(
+                "Experiment json file should contain a list of experiment objects."
+            )
 
-			extend_edate = "extend" in exp and exp["extend"]
-			offset_days = 0 if extend_edate else -(length-1)
+        for exp in experiments:
+            start, end, length = exp["sdate"], exp["edate"], exp["length"]
+            start = arrow.get(start, "YYYY-MM-DD")
+            end = arrow.get(end, "YYYY-MM-DD")
+            length = int(length)
 
-			for i, r in enumerate(arrow.Arrow.range('day', start, end.shift(days=offset_days))):
-				interval = r, r.shift(days=+(length-1), hours=+23)
-				exp_name = f'{exp["name"]}_{i}'
-				args.append((exp["path"], exp_name, interval))
-	
-	return args
+            extend_edate = "extend" in exp and exp["extend"]
+            offset_days = 0 if extend_edate else -(length - 1)
+
+            for i, r in enumerate(
+                arrow.Arrow.range("day", start, end.shift(days=offset_days))
+            ):
+                interval = r, r.shift(days=+(length - 1), hours=+23)
+                exp_name = f'{exp["name"]}_{i}'
+                run_args = [exp["path"], exp_name, interval]
+
+                if "meteo" in exp:
+                    run_args.append(exp["meteo"])
+
+                args.append(run_args)
+
+    return args
 
 
 def run_experiments(fpath: str):
-	"""Converts the JSON file at the fpath argument into a list of simulation
+    """Converts the JSON file at the fpath argument into a list of simulation
 	arguments, and executes the simulations using a multiprocessing worker pool."""
-	args = get_args(fpath)
+    args = get_args(fpath)
+
+    print("\n\n+---------------------------------+")
+    print(f'Queueing {len(args)} experiment{"s" if len(args) > 2 else ""}:')
+    for i, a in enumerate(args):
+        print(
+            f'{i:>4}.   {a[1]}    {a[2][0].format("YYYY-MM-DD HH:mm")} - {a[2][1].format("YYYY-MM-DD HH:mm")}'
+        )
+    print("+---------------------------------+\n\n")
+
+    worker_pool = Pool()
+    result = worker_pool.starmap(run_experiment, args)  # starmap unpacks args with *
+    worker_pool.close()
+    worker_pool.join()
+
+    print(f"\n{sum(result)}/{len(args)} simulations completed successfully.")
+
+    if sum(result) > 0:
+        print("\nFailed processes:")
+        for i, r in enumerate(result):
+            if not r:
+                print(f"\t{args[i][1]}")
+        print("\nCheck logs for more details.")
+
+    destination = os.path.join(os.getcwd(), f"results/")
+    print(f"Output datasets are at {destination}")
 
 
-	print('\n\n+---------------------------------+')
-	print(f'Queueing {len(args)} experiment{"s" if len(args) > 2 else ""}:')
-	for i, a in enumerate(args):
-		print(f'{i:>4}.   {a[1]}    {a[2][0].format("YYYY-MM-DD HH:mm")} - {a[2][1].format("YYYY-MM-DD HH:mm")}')
-	print('+---------------------------------+\n\n')
+if __name__ == "__main__":
+    # create CLI argument parser
+    parser = argparse.ArgumentParser(
+        description=f"Run a batch of experiments using a multiprocessing worker pool."
+        f"\n\nSee sample_experiments/ for examples."
+    )
+    parser.add_argument(
+        "json_file", type=str, help="The JSON file containing the experiments to run."
+    )
 
-	worker_pool = Pool()
-	result = worker_pool.starmap(run_experiment, args)  # starmap unpacks args with *
-	worker_pool.close()
-	worker_pool.join()
+    # parse the arguments
+    args = parser.parse_args()
 
-	print(f'\n{sum(result)}/{len(args)} simulations completed successfully.')
+    configure_logging()
+    path = sys.argv[1]
+    run_experiments(args.json_file)
 
-	if sum(result) > 0:
-		print('\nFailed processes:')
-		for i, r in enumerate(result):
-			if not r:
-				print(f'\t{args[i][1]}')
-		print('\nCheck logs for more details.')
-
-	destination = os.path.join(os.getcwd(), f'results/')
-	print(f'Output datasets are at {destination}')
-
-
-if __name__ == '__main__':
-	# create CLI argument parser
-	parser = argparse.ArgumentParser(
-		description=f'Run a batch of experiments using a multiprocessing worker pool.'
-								f'\n\nSee sample_experiments/ for examples.'
-	)
-	parser.add_argument('json_file', type=str, help="The JSON file containing the experiments to run.")
-
-  # parse the arguments
-	args = parser.parse_args()
-
-	configure_logging()
-	path = sys.argv[1]
-	run_experiments(args.json_file)
